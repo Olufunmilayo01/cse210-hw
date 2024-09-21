b20d5645600c41f78cfad07113f4a6bf57016be7
Oluwafunmilayo01 <oluwafunmilayome@gmail.comgit config --list>
2024-09-21 23:40:46 +0100

    Week 1 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // string input = Console.ReadLine();
+        // int number =int.Parse(input);
+
+
+        // Console.Write("What is your guess? ");
+        // string userInput = Console.ReadLine();
+        // int guess =int.Parse(userInput);
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        
+             if (number < guess)
+             {
+                Console.WriteLine("lower");
+             }
+
+             else if (number > guess)
+             {
+                Console.WriteLine("higher");
+             }
+              else
+             {
+            Console.WriteLine("You guessed it!");
+             }
+        }
+       
+
+        
+       
     }
 }
